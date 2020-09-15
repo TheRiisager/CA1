a0201c66d5dbd241cd68f92de1125dc026b7182c
JoakimKSS <joakim.kss@gmail.com>
2020-09-15 15:26:52 +0200

    help me

---
+++ target/generated-sources/annotations/entities/xx345y657/Member_.java
@@ -0,0 +1,16 @@
+package entities.xx345y657;
+
+import entities.Member;
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2020-09-15T15:08:32")
+@StaticMetamodel(Member.class)
+public class Member_ { 
+
+    public static volatile SingularAttribute<Member, String> fave;
+    public static volatile SingularAttribute<Member, String> name;
+    public static volatile SingularAttribute<Member, Long> id;
+
+}
