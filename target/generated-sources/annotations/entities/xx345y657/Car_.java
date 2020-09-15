f229ac6da607d1e259c16cde7ef9fdcf2c85ea77
Lasse-Emil Støvring Larsen <lforlasse@gmail.com>
2020-09-15 18:03:08 +0200

    merging git

--- target/generated-sources/annotations/entities/xx345y657/Car_.java
+++ target/generated-sources/annotations/entities/xx345y657/Car_.java
@@ -5,7 +5,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2020-09-15T15:06:15")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2020-09-15T18:01:57")
 @StaticMetamodel(Car.class)
 public class Car_ { 
 
