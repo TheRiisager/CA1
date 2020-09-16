21419459d04708533434a12ef3106528ed2560fa
Lasse-Emil Støvring Larsen <lforlasse@gmail.com>
2020-09-16 10:28:29 +0200

    fixed tracked files

--- target/CA1-1.0.1/mlg.js
+++
@@ -1,8 +0,0 @@
-window.addEventListener("click",function(){
-   document.getElementById("mlg").play();
-   
-   document.body.style.cursor = "URL(media/images/mlg.png), crosshair";
-   setTimeout(() => {document.body.style.cursor = "default";}, 200);
-});
-
-
