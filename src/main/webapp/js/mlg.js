f90a54c8afb68e1e866c585c71ab4e75dcc4567b
Frederik Johnsen <riisager@pop-os.localdomain>
2020-09-20 00:56:58 +0200

    added sick reacts to your epic clicks, gamer

--- src/main/webapp/js/mlg.js
+++ src/main/webapp/js/mlg.js
@@ -1,8 +1,29 @@
+let clickCount = 0;
+
+let audioSources = ["media/audio/noone.mp3", "media/audio/where.mp3"]
+
+
 window.addEventListener("click",function(){
    document.getElementById("mlg").play();
+
+   if(clickCount == 10){
+      document.getElementById("audiosrc").src = "media/audio/noone.mp3";
+      console.log(document.getElementById("audiosrc").currentSrc);
+      document.getElementById("reacts").load();
+      document.getElementById("reacts").play();
+   }
+
+   if(clickCount == 20){
+      document.getElementById("audiosrc").src = "media/audio/where.mp3";
+      console.log(document.getElementById("audiosrc").currentSrc);
+      document.getElementById("reacts").load();
+      document.getElementById("reacts").play();
+   }
    
    document.body.style.cursor = "URL(media/images/mlg.png), crosshair";
    setTimeout(() => {document.body.style.cursor = "default";}, 200);
+   clickCount++;
+   console.log(clickCount);
 });
 
 
